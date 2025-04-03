bc30f867191ce7c2e35b3811c97bc2d042f04435
Antoine-Developer <antoinepenadev@gmail.com>
2025-04-03 19:05:15 -0400

    Update data

--- utils/messages.py
+++ utils/messages.py
@@ -1,9 +1,8 @@
 msg_instructor = """🎬 ¡Bienvenido a tu cine personal!
 Con este bot puedes recibir 🦄películas totalmente gratis en unos pocos pasos:
 1️⃣ Pulsa "🚀Activar Acceso", después ÚNETE al GRUPO
-2️⃣ Pulsa el botón que dice 👉🏻START MINING👈🏻 y espera a que la barra de progreso de abajo se complete
-3️⃣ Regresa aquí y presiona "🛸Obtener Película".
-4️⃣ ¡Listo! La película llegará a tu chat.
+2️⃣ Regresa aquí y presiona "🛸Obtener Película".
+3️⃣ ¡Listo! La película llegará a tu chat.
 
 
 ✨ ¡Solo necesitas hacerlo una vez! Después, las películas se enviarán automáticamente. 🎥
@@ -12,7 +11,7 @@ Con este bot puedes recibir 🦄películas totalmente gratis en unos pocos pasos
 msg_soon_access = """
 🚫 ¡Acceso no permitido!
 Antes de obtener la película, necesitas completar el paso previo.
-💥 RECUERDA es escencial PULSAR EL BOTÓN DE "👉🏻START MINING 👈🏻", sino lo haces el bot no te enviará la peli
+💥 RECUERDA es escencial UNIRTE AL GRUPO, sino lo haces el bot no te enviará la peli
 
 si tienes algún problema (lo cual es raro) intenta hacer el proceso de nuevo y Listo
 """
